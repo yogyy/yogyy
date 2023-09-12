31da4f9af9537f4508029ecd65315b29ecef1840
yogyy <yogimr261@gmail.com>
2023-09-13 01:37:13 +0700

    #1 final

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,20 @@
-import { useEffect, useState } from "react";
-
-const navlink = ['home', 'blog', 'guestbook']
+const navlink = [
+  { name: 'home', link: 'yogyy.vercel.app' },
+  { name: 'blog', link: 'yogyy.vercel.app/posts' },
+  { name: 'guestbook', link: 'burungbiru.vercel.app' }
+]
 
 export default function Navbar() {
-  const [curr, setCurr] = useState(false)
-  useEffect(() => {
-    if (window !== undefined) {
-      const curr = window.location.pathname
-      setCurr(true)
-    }
-  }, []);
+
 
   return (
     <nav className="text-sm font-medium mt-8 mb-16 w-fit">
       <ul className="flex flex-row gap-3">
-        {navlink.map((nav) => (
-          <li key={nav}>
-            <a onClick={() => console.log(curr)} href={`#${nav}`}>
-              <div className="">{nav}</div>
+        {navlink.map((nav, i) => (
+          <li key={nav.name}>
+            <a className='relative' href={`https://${nav.link}`} target='_blank'>
+              {nav.name}
+              {i === 0 && <div class="absolute h-[1px] top-5 inset-0 bg-gradient-to-r from-transparent to-white w-full" />}
             </a>
           </li>
         ))}
